3c3c634b95840768d3755bffc3e22d199ee3c5c1
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-11-02 10:27:20 +0800

    add ui form post

---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,27 @@
+@extends('dashboard.layouts.main')
+@section('container')
+ <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Creeate New Posts</h1>
+    
+
+      </div>
+
+      <div class="col-lg-8">
+  <form method="post" action="/dashboard/posts">
+    @csrf 
+  <div class="mb-3">
+    <label for="title" class="form-label">Title</label>
+    <input type="text" class="form-control" id="title" name="title">
+   
+  </div>
+  <div class="mb-3">
+    <label for="slug" class="form-label">Slugs</label>
+    <input type="text" class="form-control" id="slug" name="slug">
+   
+  </div>
+
+  <button type="submit" class="btn btn-primary">Create</button>
+</form>
+      </div>
+    
+@endsection
