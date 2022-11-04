0b954f281029e4c5b7bd13a096d1206119b4298a
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-11-04 23:22:41 +0800

    preview image in form post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -45,7 +45,8 @@
 
   <div class="mb-3">
   <label for="image" class="form-label">Post Image</label>
-  <input class="form-control"  @error('image') is-invalid @enderror type="file" id="image" name="image">
+  <img class="img-preview img-fluid mb-3 col-sm-5">
+  <input class="form-control"  @error('image') is-invalid @enderror type="file" id="image" name="image" onchange="previewImage()">
   @error('image')
   <p class='text-danger'>{{$message}}</p>
   @enderror
@@ -75,5 +76,22 @@ fetch('/dashboard/posts/checkSlug?title=' +title.value)
       document.addEventListener('trix-file-accept', function(e){
         e.preventDefault();
       })
+
+      function previewImage()
+      {    
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
+        imgPreview.style.display = 'block';
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent)
+        {
+          imgPreview.src = oFREvent.target.result;
+        }
+
+      }
+
     </script>
 @endsection
