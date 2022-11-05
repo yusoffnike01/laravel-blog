be52c547efe3494f96ea6d25d63125e0f0799e2f
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-11-05 22:52:16 +0800

    add middleware

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -16,7 +16,18 @@
      
         </ul>
 
-
-       
+        @can('admin')
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Adminstrator</span>
+        </h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+             <a class="nav-link" href="/dashboard/categories" {{Request::is('dashboard/categories')? 'active': ''}}>
+              <span data-feather="grid" class="align-text-bottom"></span>
+              Post Categories
+            </a>
+          </li>
+        </ul>
+        @endcan
       </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
 
 /*
@@ -57,3 +58,4 @@ Route::get('/dashboard', function(){
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
